b233de4ad1256b736d584858e900b93400935b54
jakedesign <endlesswavyjake@gmail.com>
2021-07-17 23:09:15 -0700

    mouse cursor updates

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -1,9 +1,21 @@
 let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll('.nav-links li, .logo, .btnPrimary, .arrowBTN, .bottomItems a, .social a');
 
-window.addEventListener('mousemove', cursor);
+window.addEventListener('mousemove',cursor);
 
-function cursor(e){
-    mouseCursor.style.top = e.pageY + "px";
-    mouseCursor.style.left = e.pageX + "px";
+function cursor(e) {
+    mouseCursor.style.top = e.pageY + 4 + 'px';
+    mouseCursor.style.left = e.pageX + 4 + 'px';
 }
 
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        mouseCursor.classList.add('link-grow')
+    });
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseleave', () => {
+        mouseCursor.classList.remove('link-grow')
+    });
+});
