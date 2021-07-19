3bbb9ee39a482cd18137725d1b7347e551ee18c8
jakedesign <endlesswavyjake@gmail.com>
2021-07-18 21:27:08 -0700

    revert to last night code
    unsure how "hover me" button broke

--- javascript/app.js
+++ javascript/app.js
@@ -3,8 +3,6 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
     
-    const swup = new Swup()
-
     burger.addEventListener('click',() => {
     //Toggle Nav
     nav.classList.toggle('nav-active');
@@ -23,10 +21,11 @@ const navSlide = () => {
 
 navSlide();
 
-
 $(".btnPrimary").mouseenter(function() {
     $(".hero-statement").text("creator ✍🏽  designer 🎨 plant dad 🌱 snowboarder 🏂 music connoisseur 🎵 pnw wanderer🌲 wood worker 📐 oatmilk lattes ☕️ beliver in aliens 👽 and continually working to sharpen my craft");
+
 })
+
 .mouseleave(function() {
     $(".hero-statement").text("A budding UX/UI designer with a passion for using his brain cells 🧠 to create design experiences that are meaningful, intuitive, and purposeful!");
 })
