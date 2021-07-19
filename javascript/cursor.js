d4118d3ad021852fdd86f7a42b0a21df1d5ea50b
jakedesign <endlesswavyjake@gmail.com>
2021-07-18 21:19:42 -0700

    changes

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -1,21 +1,13 @@
 let mouseCursor = document.querySelector(".cursor");
+<<<<<<< HEAD
 let navLinks = document.querySelectorAll('.nav-links li, .logo, .btnPrimary, .arrowBTN, .bottomItems a, .social a, .content-box3 a, .contactbox-2 a');
+=======
+>>>>>>> parent of b233de4 (mouse cursor updates)
 
-window.addEventListener('mousemove',cursor);
+window.addEventListener('mousemove', cursor);
 
-function cursor(e) {
-    mouseCursor.style.top = e.pageY + 4 + 'px';
-    mouseCursor.style.left = e.pageX + 4 + 'px';
+function cursor(e){
+    mouseCursor.style.top = e.pageY + "px";
+    mouseCursor.style.left = e.pageX + "px";
 }
 
-navLinks.forEach(link => {
-    link.addEventListener('mouseover', () => {
-        mouseCursor.classList.add('link-grow')
-    });
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('mouseleave', () => {
-        mouseCursor.classList.remove('link-grow')
-    });
-});
